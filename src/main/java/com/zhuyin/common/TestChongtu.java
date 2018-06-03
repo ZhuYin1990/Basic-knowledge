4a868125cdc7c788a6337361ea80bd41d89ca1f0
Administrator <Administrator@PC201804061205>
2018-06-03 22:36:39 +0800

    解决冲突测试

---
+++ src/main/java/com/zhuyin/common/TestChongtu.java
@@ -0,0 +1,22 @@
+package com.zhuyin.common;
+/**
+ * <p>Title: TestChongtu.java</p>
+ * <p>Description: </p>
+ * <p>Company: </p>
+ * @author ZY
+ * <p> Just go on !!!</p>
+ * @date 2018年6月3日  下午10:35:17 
+ * @version v1.0
+ */
+public class TestChongtu {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("这是程序猿A写的代码");
+		
+		
+	}
+	
+}
+
